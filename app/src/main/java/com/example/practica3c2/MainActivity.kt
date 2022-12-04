45080079e5f629939a6f25f5d41ddf82002e9b18
Pacoalberola <falberola2003@gmail.com>
2022-12-04 21:23:50 +0100

    Commit codigo toolbar

--- app/src/main/java/com/example/practica3c2/MainActivity.kt
+++ app/src/main/java/com/example/practica3c2/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.practica3c2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
